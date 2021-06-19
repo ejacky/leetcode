1fc89faa77c3b77c448e8250ec5e821b4f81974e
jackyzhang <852566802@qq.com>
2021-06-19 21:21:32 +0800

    优化

--- 1.two-sum/solution_test.go
+++ 1.two-sum/solution_test.go
@@ -11,13 +11,3 @@ func ExampleTwoSum() {
 	//[1 2]
 	//[0 1]
 }
-
-func ExampleTwoSum1() {
-	fmt.Println(twoSum1([]int{2, 7, 11, 5}, 9))
-	fmt.Println(twoSum1([]int{3, 2, 4}, 6))
-	fmt.Println(twoSum1([]int{3, 3}, 6))
-	//output:
-	//[0 1]
-	//[1 2]
-	//[0 1]
-}
