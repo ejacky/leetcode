283ab1b7188e8485f5ee43bb7da6cd59f9084181
jackyzhang <852566802@qq.com>
2021-06-23 07:49:48 +0800

    添加官方解法

--- 210.course-schedule-ii/solution.go
+++ 210.course-schedule-ii/solution.go
@@ -77,3 +77,83 @@ func findOrder(numCourses int, prerequisites [][]int) []int {
 	return order
 
 }
+
+func findOrder1(numCourses int, prerequisites [][]int) []int {
+	var (
+		edges   = make([][]int, numCourses)
+		visited = make([]int, numCourses)
+		result  []int
+		valid   bool = true
+		dfs     func(u int)
+	)
+
+	dfs = func(u int) {
+		visited[u] = 1
+		for _, v := range edges[u] {
+			if visited[v] == 0 {
+				dfs(v)
+				if !valid {
+					return
+				}
+			} else if visited[v] == 1 {
+				valid = false
+				return
+			}
+		}
+		visited[u] = 2
+		result = append(result, u)
+	}
+
+	for _, info := range prerequisites {
+		edges[info[1]] = append(edges[info[1]], info[0])
+	}
+
+	for i := 0; i < numCourses && valid; i++ {
+		if visited[i] == 0 {
+			dfs(i)
+		}
+	}
+	if !valid {
+		return []int{}
+	}
+	for i := 0; i < len(result)/2; i++ {
+		result[i], result[numCourses-i-1] = result[numCourses-i-1], result[i]
+	}
+	return result
+}
+
+func findOrder2(numCourses int, prerequisites [][]int) []int {
+	var (
+		edges  = make([][]int, numCourses)
+		indeg  = make([]int, numCourses)
+		result []int
+	)
+
+	for _, info := range prerequisites {
+		edges[info[1]] = append(edges[info[1]], info[0])
+		indeg[info[0]]++
+	}
+
+	q := []int{}
+	for i := 0; i < numCourses; i++ {
+		if indeg[i] == 0 {
+			q = append(q, i)
+		}
+	}
+
+	for len(q) > 0 {
+		u := q[0]
+		q = q[1:]
+		result = append(result, u)
+		for _, v := range edges[u] {
+			indeg[v]--
+			if indeg[v] == 0 {
+				q = append(q, v)
+			}
+		}
+	}
+	if len(result) != numCourses {
+		return []int{}
+	}
+	return result
+}
