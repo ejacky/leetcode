0a79cb8239791874bd20554810c558b561ba96ae
jackyzhang <852566802@qq.com>
2021-06-28 07:24:11 +0800

    100

--- 100.same-tree/solution.go
+++ 100.same-tree/solution.go
@@ -7,46 +7,49 @@ type TreeNode struct {
 }
 
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-	return false
+
+	var flag = true
+
+	var preorder func(tree1 *TreeNode, tree2 *TreeNode)
+	preorder = func(tree1 *TreeNode, tree2 *TreeNode) {
+		if tree1 != nil && tree2 != nil {
+			if tree1.Val != tree2.Val && flag {
+				flag = false
+			} else {
+				preorder(tree1.Left, tree2.Left)
+				preorder(tree1.Right, tree2.Right)
+			}
+		}
+		if tree1 == nil && tree2 != nil || tree1 != nil && tree2 == nil {
+			flag = false
+		}
+	}
+	preorder(p, q)
+	return flag
 }
 
-func createTree(nums []int) *TreeNode {
+func leverInsert(nums []int) *TreeNode {
 	var root *TreeNode
-	if len(nums) == 0 {
-		return root
-	}
+	root = &TreeNode{Val: nums[0]}
 
-	var insert func(tree *TreeNode, num int)
-	insert = func(tree *TreeNode, num int) {
-
-		if num < tree.Val {
-			if tree.Left == nil {
-				tree.Left = &TreeNode{Val: num}
-			} else {
-				insert(tree.Left, num)
-			}
+	var treeQueue []*TreeNode
+	treeQueue = append(treeQueue, root)
+	for i := 1; i <= len(nums)-1; i++ {
+		tree := treeQueue[0]
+		treeQueue = treeQueue[1:]
+		if nums[i] != -1 {
+			tree.Left = &TreeNode{Val: nums[i]}
+			treeQueue = append(treeQueue, tree.Left)
+		}
+		if i+1 > len(nums)-1 {
+			break
 		}
 
-		if num > tree.Val {
-			if tree.Right == nil {
-				tree.Right = &TreeNode{Val: num}
-			} else {
-				insert(tree.Right, num)
-			}
+		if nums[i+1] != -1 {
+			tree.Right = &TreeNode{Val: nums[i+1]}
+			treeQueue = append(treeQueue, tree.Right)
 		}
-	}
-
-	root = &TreeNode{}
-	root.Val = nums[0]
-
-	var queueNode []TreeNode
-
-	for i := 0; i < len(nums); i++ {
-		queueNode = append(queueNode, TreeNode{Val: nums[i]})
-	}
-
-	for _, num := range nums[1:] {
-		insert(root, num)
+		i++
 	}
 
 	return root
--- 100.same-tree/solution_test.go
+++ 100.same-tree/solution_test.go
@@ -3,12 +3,11 @@ package _00_same_tree
 import "fmt"
 
 func ExampleSameTree() {
-	fmt.Println(isSameTree(createTree([]int{1, 2, 3}), createTree([]int{1, 2, 3})))
-	fmt.Println(isSameTree(createTree([]int{1, 2, 3}), createTree([]int{1, -1, 2})))
-	fmt.Println(isSameTree(createTree([]int{1, 2, 1}), createTree([]int{1, 1, 2})))
-
+	fmt.Println(isSameTree(leverInsert([]int{1, 2, 3}), leverInsert([]int{1, 2, 3})))
+	fmt.Println(isSameTree(leverInsert([]int{1, 2}), leverInsert([]int{1, -1, 2})))
+	fmt.Println(isSameTree(leverInsert([]int{1, 2, 1}), leverInsert([]int{1, 1, 2})))
 	//output:
-	//[0 1]
-	//[1 2]
-	//[0 1]
+	//true
+	//false
+	//false
 }
