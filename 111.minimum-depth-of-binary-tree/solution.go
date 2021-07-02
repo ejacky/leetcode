cff00627b0f9279e1dfdf729363970a5f08bb094
jackyzhang <852566802@qq.com>
2021-07-02 08:39:20 +0800

    暂存

--- 111.minimum-depth-of-binary-tree/solution.go
+++ 111.minimum-depth-of-binary-tree/solution.go
@@ -1,5 +1,7 @@
 package _11_minimum_depth_of_binary_tree
 
+import "math"
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -7,19 +9,95 @@ type TreeNode struct {
 }
 
 func minDepth(root *TreeNode) int {
-	var depth int
-	var invert func(root *TreeNode)
+	if root == nil {
+		return 0
+	}
+	if root.Left == nil && root.Right == nil {
+		return 1
+	}
+	minD := math.MaxInt32
+	if root.Left != nil {
+		minD = min(minDepth(root.Left), minD)
+	}
+	if root.Right != nil {
+		minD = min(minDepth(root.Right), minD)
+	}
+	return minD + 1
+}
 
-	invert = func(root *TreeNode) {
-		if root != nil {
-			invert(root.Left)
-			invert(root.Right)
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func minDepth2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	queue := []*TreeNode{}
+	count := []int{}
+	queue = append(queue, root)
+	count = append(count, 1)
+	for i := 0; i < len(queue); i++ {
+		node := queue[i]
+		depth := count[i]
+		if node.Left == nil && node.Right == nil {
+			return depth
+		}
+		if node.Left != nil {
+			queue = append(queue, node.Left)
+			count = append(count, depth+1)
+		}
+		if node.Right != nil {
+			queue = append(queue, node.Right)
+			count = append(count, depth+1)
 		}
 	}
+	return 0
+}
 
-	invert(root)
-	return depth
+func minDepth_tmp(root *TreeNode) int {
+	var finished bool
+	var level func(root *TreeNode, depth int)
+	var hasLeft, hasRight bool
 
+	level = func(root *TreeNode, depth int) {
+		if root != nil && !finished {
+
+			if root.Left != nil {
+				hasLeft = true
+
+			}
+			level(root.Left, depth+1)
+			if root.Right != nil {
+				hasRight = true
+
+			}
+			level(root.Right, depth+1)
+		} else if root == nil && hasLeft && hasRight {
+			finished = true
+		}
+
+		return
+	}
+
+	level(root, 0)
+	return 1
+
+}
+
+func dfs(root *TreeNode, index int, res *[][]int) {
+	if root == nil {
+		return
+	}
+	if index == len(*res) {
+		*res = append(*res, []int{})
+	}
+	(*res)[index] = append((*res)[index], root.Val)
+	dfs(root.Left, index+1, res)
+	dfs(root.Right, index+1, res)
 }
 
 func leverInsert(nums []int) *TreeNode {
